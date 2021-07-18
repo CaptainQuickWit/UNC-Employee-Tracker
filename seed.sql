356b4bf5824096f5ad606597de72c9dd522d0149
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-18 01:43:41 -0400

    updated the seeds.sql file

--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
-USE employees_db;
+USE employee_db;
 
 
 INSERT INTO department (id, name)
@@ -9,55 +9,76 @@ INSERT INTO department (id, name)
 VALUES (3, "Dispatch");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES ("Video Repair", 20000, 1);
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Billing", 20000, 1);
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Sales", 100000, 1);
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Internet Repair", 20000, 1);
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Mobile Billing", 20000, 1);
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Mobile Repair", 20000, 1);
+VALUES (1, "Video Repair", 20000 ,1);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES ("In home technician", 20000, 2);
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Maintaince technician", 20000, 2);
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Headend technician", 25000, 2);
-INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Billing", 20000,1);
 
-VALUES ("Dispatch help desk", 15000, 3);
 INSERT INTO role (id, title, salary, department_id)
-VALUES ("DOJ", 15000, 3);
+VALUES (3, "Sales", 100000 ,1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, "Internet Repair", 20000 ,1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, "Mobile Billing", 20000 ,1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, "Mobile Repair", 20000 ,1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, "In home technician", 20000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, "Maintaince technician", 20000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (9, "Headend technician", 25000 , 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (10, "Dispatch help desk", 15000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (11, "DOJ", 15000, 3);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (1,"James", "Smith", 1, 11);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (2,"James", "Carter", 1, 11);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (3,"Michael", "Smith", 2, 22);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (4,"Michael", "Carter", 2, 22);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (5,"Jordan", "Smith", 3, 33);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (6,"Jordan", "Carter", 3, 33);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (7,"Henry", "Smith", 4, 44);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (8,"Jack", "Smith", 5, 55);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (9,"Joe", "Smith", 6, 66);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (10,"Chris", "Smith", 7, 77);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (11,"Liam", "Smith", 8, 88);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (12,"Benjamin", "Smith", 9, 99);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (13,"Alex", "Smith", 10, 110);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (14,"Ethan", "Smith", 11, 121);
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const mysql = require('mysql');
+
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
-const app = express();
+
 
 
 const connection = mysql.createConnection({
@@ -19,6 +19,139 @@ const connection = mysql.createConnection({
   database: 'employee_db',
 });
 
+/**===================DELETE ME=========== */
+connection.connect((err) => {
+    if (err) throw err;
+    runSearch();
+  });
+  
+
+const runSearch = () => {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+          'View All Employees',
+          'View Employees by Department',
+          'View Employees by Role',
+          'Add Department',
+          'Add Role',
+          'Add Employee',
+          'Update Employee'
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View All Employees':
+            allEmployees();
+            break;
+  
+          case 'View Employees by Department':
+            departmentSearch();
+            break;
+  
+          case 'View Employees by Role':
+            roleSearch();
+            break;
+  
+          case 'Add Department':
+            addDepartment();
+            break;
+  
+          case 'Add Role':
+            addRole();
+            break;
+  
+          case 'Add Employee':
+            addEmployee();
+            break;
+          
+          case 'Update Employee':
+            updateEmployee();
+            break;
+  
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+        }
+      });
+  };
+  
+  const allEmployees = () => {
+    const query = 
+    'SELECT first_name,last_name FROM employee';
+    connection.query(query, (err, res) => {
+      if (err) throw (err);
+      res.forEach(({ first_name, last_name}) =>
+      console.log(
+        `First Name: ${first_name} || Last Name: ${last_name}`
+        )
+      );
+      runSearch();
+    });
+  };
+  
+  const departmentSearch = () => {
+    inquirer
+      .prompt({
+        name:'department',
+        type: 'input'
+      })
+      .then((answer) => {
+        const query = 'SELECT first_name, last_name, '
+        //NEED TO JOIN TO GET INFO FROM OTHER TABLE FOR DEPARTMENT
+        connection.query(query, {})
+
+      }
+      );
+  };
+  
+  const roleSearch = () => {
+  
+  };
+  
+  const addDepartment = () => {
+    inquirer
+      .prompt([
+        {
+          name: 'addDepartment',
+          type: 'input',
+          message: 'What department would you like to add?',
+        }
+      .then((answer) => {
+        const query =
+        'INSERT INTO department(department_name) VALUES ?';
+        connection.query(query, [answer.addDepartment], (err, res) => {
+          if (err) throw err;
+          console.log(res);
+          runSearch();
+        })
+      })
+      ])
+  };
+  
+  const addRole = () => {
+    inquirer
+      .prompt([
+        {
+          name: 'addRole',
+          type: 'input',
+          message: 'What role would you like to add?',
+        }
+      .then((answer) => {
+        const query =
+        'INSERT INTO role(title) VALUES ?';
+        connection.query(query, [answer.addRole], (err, res) => {
+          if (err) throw (err);
+          console.log(res);
+          runSearch();
+        })
+      })
+      ])
+  
+  };
 
 
-
+/**===================DELETE ME=========== */
