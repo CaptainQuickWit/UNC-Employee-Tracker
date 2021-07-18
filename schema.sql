ff1b7853707072d14253f0ff778722c0937255cd
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-18 02:45:12 -0400

    had issues with schema, its fixed and databases can be seeded

--- schema.sql
+++ schema.sql
@@ -1,33 +1,25 @@
-
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
+
 USE employee_db;
 
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR (30),
-    last_name VARCHAR (30),
-    role_id INT (20),
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    manager_id INT (20),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
-    PRIMARY KEY (id),
-);
-
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30),
-    PRIMARY KEY (id),
+	department_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE 
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL (10,2),  
-    department_id INT (20),
-    FOREIGN KEY (department_id) REFERENCES department(id),
-    PRIMARY KEY (id),
+    salary DECIMAL(10),
+    FOREIGN KEY (role_id) REFERENCES department(department_id)
 );
 
-/*SELECT <== do i really need this..?*/ 
+CREATE TABLE employee (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    FOREIGN KEY (employee_id) REFERENCES role(role_id),
+    manager_id INT
+);
 
--- seed.sql
+++ seed.sql
@@ -1,84 +1,86 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
 USE employee_db;
 
+CREATE TABLE department (
+	department_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE 
+);
 
-INSERT INTO department (id, name)
-VALUES (1, "Customer Operations");
-INSERT INTO department (id, name)
-VALUES (2, "Field Operations");
-INSERT INTO department (id, name)
-VALUES (3, "Dispatch");
+CREATE TABLE job (
+    job_id INT AUTO_INCREMENT PRIMARY KEY,
+    dept_id_FK INT,
+    title VARCHAR(30),
+    salary DECIMAL(10),
+    FOREIGN KEY (dept_id_FK) REFERENCES department(department_id)
+);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Video Repair", 20000 ,1);
+CREATE TABLE employee (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    job_id_FK INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    FOREIGN KEY (job_id_FK) REFERENCES job(job_id),
+    manager_id INT
+);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (2, "Billing", 20000,1);
+INSERT INTO department (name)
+VALUES ("Customer Operations");
+INSERT INTO department (name)
+VALUES ("Field Operations");
+INSERT INTO department (name)
+VALUES ("Dispatch");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (3, "Sales", 100000 ,1);
+INSERT INTO job (title, salary)
+VALUES ("Video Repair", 20000);
+INSERT INTO job (title, salary)
+VALUES ("Billing", 20000);
+INSERT INTO job (title, salary)
+VALUES ("Sales", 100000);
+INSERT INTO job ( title, salary)
+VALUES ("Internet Repair", 20000);
+INSERT INTO job ( title, salary)
+VALUES ("Mobile Billing", 20000);
+INSERT INTO job ( title, salary)
+VALUES ("Mobile Repair", 20000);
+INSERT INTO job ( title, salary)
+VALUES ("In home technician", 20000);
+INSERT INTO job ( title, salary)
+VALUES ("Maintaince technician", 20000);
+INSERT INTO job ( title, salary)
+VALUES ("Headend technician", 25000);
+INSERT INTO job ( title, salary)
+VALUES ("Dispatch help desk", 15000);
+INSERT INTO job ( title, salary)
+VALUES ("DOJ", 15000);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (4, "Internet Repair", 20000 ,1);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("James", "Smith", 11);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("James", "Carter",  11);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Michael", "Smith",  22);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Michael", "Carter", 22);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Jordan", "Smith", 33);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Jordan", "Carter",  33);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Henry", "Smith", 44);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Jack", "Smith",  55);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Joe", "Smith",66);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Chris", "Smith",  77);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Liam", "Smith",  88);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Benjamin", "Smith",  99);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Alex", "Smith", 110);
+INSERT INTO employee ( first_name, last_name,  manager_id)
+VALUES ("Ethan", "Smith", 121);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (5, "Mobile Billing", 20000 ,1);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (6, "Mobile Repair", 20000 ,1);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (7, "In home technician", 20000, 2);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (8, "Maintaince technician", 20000, 2);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (9, "Headend technician", 25000 , 2);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (10, "Dispatch help desk", 15000, 3);
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (11, "DOJ", 15000, 3);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1,"James", "Smith", 1, 11);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2,"James", "Carter", 1, 11);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (3,"Michael", "Smith", 2, 22);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (4,"Michael", "Carter", 2, 22);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (5,"Jordan", "Smith", 3, 33);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (6,"Jordan", "Carter", 3, 33);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (7,"Henry", "Smith", 4, 44);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (8,"Jack", "Smith", 5, 55);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (9,"Joe", "Smith", 6, 66);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (10,"Chris", "Smith", 7, 77);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (11,"Liam", "Smith", 8, 88);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (12,"Benjamin", "Smith", 9, 99);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (13,"Alex", "Smith", 10, 110);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (14,"Ethan", "Smith", 11, 121);
